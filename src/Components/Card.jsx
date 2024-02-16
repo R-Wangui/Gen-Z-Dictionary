acb1ee5d5ec330e1da1ef545f92c8cd8aecc4aee
Rachael Njoroge <wangoinjoroge450@gmail.com>
2024-02-16 17:45:32 +0300

    Updated the UI after the .map() method was added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,13 +3,13 @@
 function Card(props) {
   return (
     <>
-        <div className="container">
-            <div className="card">
-                <h3>{props.title}</h3>
-                <p>{props.meaning}</p>
-                <p className="use" >{props.use}</p>
-            </div>
-        </div>  
+      <div className="container">
+        <div className="card">
+          <h3>{props.title}</h3>
+          <p>{props.meaning}</p>
+          <p className="use" >{props.use}</p>
+        </div>
+      </div>  
     </>
   )
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,8 +8,9 @@ function Header() {
             <h1>GEN-Z DICTIONARY</h1>
         </div> 
         <div className="searchbar">
-            <input type="search" className="search" placeholder="Search" />   
-            <input type="search" className="add" placeholder="Add New Words" />
+            <input type="search" className="search" placeholder="Search" /> 
+            <button className="button" >Add New Words</button>  
+            {/* <input type="search" className="add" placeholder="Add New Words" /> */}
         </div>
         
         
